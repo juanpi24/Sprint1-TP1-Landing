f875a0f97310aa7ee2d21ff8ac3d58643c26092e
Juan Pablo Millicay <juan22milli@gmail.com>
2026-08-20 12:31:32 -0300

    componente app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-function App() {
- 
+// src/App.jsx
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Benefits from "./components/Benefits";
+import Courses from "./components/Courses";
+import ContactForm from "./components/ContactForm";
+import Footer from "./components/Footer";
+
+export default function App() {
   return (
     <>
-      <h1>Revision</h1>
+      <Navbar />
+      <main className="pb-16 pt-20">
+        <Hero />
+        <Benefits />
+        <Courses />
+        <ContactForm />
+      </main>
+      <Footer />
     </>
-  )
+  );
 }
-
-export default App
